c51cb804fb581e633a51b4d09e1ca5967bf36a97
Yuki Kita <kita.yuki@gmail.com>
2010-02-17 21:54:55 +0900

    Improved perfomance

--- app/models/morale.rb
+++ app/models/morale.rb
@@ -10,11 +10,13 @@ class Morale < Feeling
   end
   def level
     return nil unless @feelings
+    return @cached_level if @cached_level
     average = @feelings.inject(0) {|result, feeling| result += feeling.level} / @feelings.size.to_f
     count = FEELING_TYPES.size
     1.upto(count) do |i|
       if average <= ((count - 1) / count.to_f) * i
-        return (i - 1)
+        @cached_level = (i - 1)
+        return @cached_level
       end
     end
     raise "must not happen"
