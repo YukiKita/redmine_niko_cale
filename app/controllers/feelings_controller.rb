5ad1d23d8f10545e67378464a8ae73542e030278
Yuki Kita <kita.yuki@gmail.com>
2010-02-20 00:53:46 +0900

    add feelings controller

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -2,17 +2,10 @@ class FeelingsController < ApplicationController
   unloadable
   helper :niko_cale
   def show
-    @feeling = find_feeling
-    unless @feeling
-      render_404
-    end
-  end
-
-  def find_feeling
     begin
-      Feeling.find(params[:id])
+      @feeling = Feeling.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      nil
+      render_404
     end
   end
 end
