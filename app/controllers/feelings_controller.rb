5cd1ae683cc3d4a16eab89d49966e37d15ba5a0e
Yuki Kita <kita.yuki@gmail.com>
2010-02-20 09:34:14 +0900

    add feelings controller

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,6 +1,9 @@
 class FeelingsController < ApplicationController
   unloadable
   helper :niko_cale
+  def index
+    @feelings = Feeling.find([2, 4])
+  end
   def show
     begin
       @feeling = Feeling.find(params[:id])
