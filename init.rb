8fca8543caa7a0de5ed6817c19b1694833daaf07
Yuki Kita <kita.yuki@gmail.com>
2010-03-22 17:53:02 +0900

    url added

--- init.rb
+++ init.rb
@@ -19,6 +19,7 @@ Redmine::Plugin.register :redmine_niko_cale do
   name 'Niko-niko Calender plugin'
   author 'Yuki Kita'
   description 'This is a plugin that makes member\'s feeling visible'
+  url         'http://github.com/YukiKita/redmine_niko_cale'
   version '0.9.2'
   requires_redmine :version_or_higher => '0.9.0'
 
