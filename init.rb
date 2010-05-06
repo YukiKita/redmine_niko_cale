ee73809833b1698226302240dca07b8fc5d5120f
Yuki Kita <kita.yuki@gmail.com>
2010-05-06 23:33:55 +0900

    Changed UI

--- init.rb
+++ init.rb
@@ -20,7 +20,7 @@ Redmine::Plugin.register :redmine_niko_cale do
   author 'Yuki Kita'
   description 'This is a plugin that shows the mood of project members and the overall project on a daily basis'
   url         'http://github.com/YukiKita/redmine_niko_cale'
-  version '0.9.11'
+  version '0.9.12'
   requires_redmine :version_or_higher => '0.9.0'
 
   project_module :niko_cale do
