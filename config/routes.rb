f656b31e049560627e6b8b0129954a306ac973d8
YOSHITANI Mitsuhiro <luckval@gmail.com>
2012-07-24 01:00:23 +0900

    support new routes for redmine-2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:project_id/niko_cale', :controller => 'niko_cale', :action => 'index'
-  map.connect 'feelings/edit_comment/:id', :controller => 'feelings', :action => 'edit_comment'
-  map.connect 'niko_cale_settings/preview', :controller => 'niko_cale_settings', :action => 'preview'
-  map.resources :feelings 
+RedmineApp::Application.routes.draw do
+  match 'projects/:project_id/niko_cale', :to=> 'niko_cale#index'
+  match 'feelings/edit_comment/:id', :to => 'feelings#edit_comment'
+  match 'niko_cale_settings/preview', :to => 'niko_cale_settings#preview'
+  resources :feelings 
 end
